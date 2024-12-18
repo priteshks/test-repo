32e3ed9a77e93da1f8b368e114371077a7e4a9d6
Priteshkumar Singh <147624773+priteshks@users.noreply.github.com>
2024-12-18 17:23:43 +0530

    add env variable to script

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// index.js
 const core = require('@actions/core');
 
 try {
-  const input = core.getInput('log-message');
+  // Get the log message from the environment variable
+  const input = process.env.LOG_MESSAGE;
+  if (!input) {
+    throw new Error('LOG_MESSAGE environment variable is not set.');
+  }
   const reversedMessage = input.split('').reverse().join('');
   console.log(`Reversed Message: ${reversedMessage}`);
 } catch (error) {
